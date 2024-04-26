c1ffa560881837a0053f7e138d063312225aab56
Philip Minakov <sir.cadogan@protonmail.com>
2024-04-26 20:45:04 +0400

    Add theme elements

--- patch.c
+++ patch.c
@@ -23,7 +23,9 @@ int Entry(HObj hobj, int *r8, WSHDR *path) {
         if (theme_cache_id == SELECTION_1_LINE ||
             theme_cache_id == SELECTION_2_LINE ||
             theme_cache_id == SELECTION_3_LINE ||
-            theme_cache_id == TAB_SELECTED) {
+            theme_cache_id == TAB_SELECTED ||
+            theme_cache_id == POPUP_SEARCH_LINE ||
+            theme_cache_id == POPUP_QUICK_ACCESS_FIELD ) {
             _Obs_SetTransparent(hobj, 5);
         }
     }
