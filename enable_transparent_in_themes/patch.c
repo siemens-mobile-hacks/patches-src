cbee74df3f353e0229b66fa50495a683baeb689c
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2024-11-20 13:27:12 +0000

    cmake + port ot EL71v45

--- enable_transparent_in_themes/patch.c
+++ enable_transparent_in_themes/patch.c
@@ -6,17 +6,20 @@
         #ifdef E71_45
             #include "E71_45.h"
         #endif
+        #ifdef EL71_45
+            #include "EL71_45.h"
+        #endif
     #else
     #endif
 #endif
 
 
-#define GetThemeCacheIdByUnk ((int (*)(unsigned int))(0xa073ccec))
+#define GetThemeCacheIdByUnk ((int (*)(unsigned int))(ADDR_GetThemeCacheIdByUnk))
 #define _Obs_SetInput_File ((int (*)(HObj, int *r8, WSHDR *path))(ADDR_Obs_SetInput_File))
 #define _Obs_SetTransparent ((int (*)(HObj, int))(ADDR_Obs_SetTransparent))
 
-__attribute__((section(".text.Entry")))
-int Entry(HObj hobj, int *r8, WSHDR *path) {
+__attribute__((section(".text.HookThemeImageLoading")))
+int HookThemeImageLoading(HObj hobj, int *r8, WSHDR *path) {
     int theme_cache_id = GetThemeCacheIdByUnk(*(char*)(*r8 + sizeof(int)));
     int res = _Obs_SetInput_File(hobj, 0, path);
     if (res == 0) {
