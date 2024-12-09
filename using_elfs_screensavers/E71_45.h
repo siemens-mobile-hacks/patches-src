5ccee4d73fc952922d689c6aaacfce50588ac59f
Philip Minakov <sir.cadogan@protonmail.com>
2024-12-09 23:22:07 +0400

    E71_45

---
+++ using_elfs_screensavers/E71_45.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define ADDR_CreateGUI         (0xA094F52F)
+#define ADDR_GBS_SendMessage   (0xA0091EAB)
+#define ADDR_str_2ws           (0xA0568AB0)
+#define ADDR_GetLangMessIntoWS (0xA08D5F30)
+
+#define LGP_ID_ENERGY_SAVER    0x342
--- using_elfs_screensavers/patch.c
+++ using_elfs_screensavers/patch.c
@@ -1,6 +1,9 @@
 #include <swilib.h>
 
 #ifdef NEWSGOLD
+    #ifdef E71_45
+        #include "E71_45.h"
+    #endif
 #else
     #define SCREEN_W 132
     #define SCREEN_H 176
@@ -11,29 +14,38 @@
 
 #define _CreateGUI ((int (*)(GUI *gui))(ADDR_CreateGUI))
 #define _GBS_SendMessage ((void (*)(int cepid, int msg, ...))(ADDR_GBS_SendMessage))
-#define _DrawRectangle ((void (*)(int, int, int, int, int, const char *, const char *))(ADDR_DrawRectangle))
-#define _GetPaletteAdrByColorIndex ((char* (*)(int index))(ADDR_GetPaletteAdrByColorIndex))
 #define _GetLangMessIntoWS ((void (*)(int lgp_id, WSHDR *ws))(ADDR_GetLangMessIntoWS))
 #define _str_2ws ((void (*)(WSHDR *ws, const char *str, unsigned int size))(ADDR_str_2ws))
 
-#define SCREENSAVER_ELF SCREENSAVER_ENERGY_SAVER
+#ifndef NEWSGOLD
+    #define _DrawRectangle ((void (*)(int, int, int, int, int, const char *, const char *))(ADDR_DrawRectangle))
+    #define _GetPaletteAdrByColorIndex ((char* (*)(int index))(ADDR_GetPaletteAdrByColorIndex))
+#endif
 
 __attribute__((target("thumb")))
 __attribute__((section(".text.CreateGUI")))
 int CreateGUI_SS(GUI *gui) {
     int gui_id = _CreateGUI(gui);
+#ifdef NEWSGOLD
+    if ((unsigned int)gui->definition >= 0xA8000000) {
+        _GBS_SendMessage(MMI_CEPID, 0x3FA, gui_id, gui);
+    }
+#else
     if (gui->definition == DEFINITON_ENERGY_SAVER) {
         _GBS_SendMessage(MMI_CEPID, 0x3FA, gui_id, gui);
     }
+#endif
     return gui_id;
 }
 
+#ifndef NEWSGOLD
 __attribute__((target("thumb")))
 __attribute__((section(".text.OnRedraw")))
 void OnRedraw() {
     _DrawRectangle(0, 0, SCREEN_W - 1, SCREEN_H - 1, 0,
         _GetPaletteAdrByColorIndex(1), _GetPaletteAdrByColorIndex(1));
 }
+#endif
 
 __attribute__((target("thumb")))
 __attribute__((section(".text.GetLangMessageIntoWS")))
