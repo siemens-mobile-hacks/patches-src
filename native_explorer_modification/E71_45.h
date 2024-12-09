3405a29723e83e332715a5211af82d7a933c6aee
Philip Minakov <sir.cadogan@protonmail.com>
2024-12-09 12:24:16 +0400

    Add patch

---
+++ native_explorer_modification/E71_45.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define ADDR_wsprintf           (0xA093EB4D)
+#define ADDR_GetPITaddr         (0xA094CE9B)
+#define ADDR_CalcBitmapSize     (0xA094CF5D)
+
+#define ADDR_MergeIMGHDR       (0xA094D0DC | 1)
+#define ADDR_MergeIcons_Unk    (0xA0718A70)
+#define ADDR_SetPreview        (0xA0718E3C)
+#define ADDR_GHook_SendMessage (0xA06987D4)
---
+++ native_explorer_modification/patch.c
@@ -0,0 +1,37 @@
+#include <swilib.h>
+
+#ifdef E71_45
+    #include "E71_45.h"
+#endif
+
+#define _wsprintf ((void (*)(WSHDR *, const char *, ...))(ADDR_wsprintf))
+#define _GetPITaddr ((IMGHDR *(*)(int num))(ADDR_GetPITaddr))
+#define _CalcBitmapSize ((int (*)(short w, short i, char bpnum))(ADDR_CalcBitmapSize))
+
+
+#define MergeIMGHDR ((void (*)(IMGHDR *src, IMGHDR *dest))(ADDR_MergeIMGHDR))
+#define MergeIcons_Unk ((void (*)(void *, void *))(ADDR_MergeIcons_Unk))
+#define SetPreview ((void (*)(void *))(ADDR_SetPreview))
+
+__attribute__((target("thumb")))
+__attribute__((section(".text.MergeIconsHook")))
+void MergeIconsHook(void *r0, void *r1, IMGHDR *src) {
+    if (((char*)r0)[0x84]) { // enabled mark mode
+        src = _GetPITaddr(1336);
+    }
+    IMGHDR *dest = (IMGHDR*)((char*)r1 + 8);
+    MergeIMGHDR(src, dest);
+    MergeIcons_Unk(r0, r1);
+}
+
+__attribute__((section(".text.SetPreviewHook")))
+void SetPreviewHook(void *r0) {
+    if (!((char*)r0)[0x84]) { // disabled mark mode
+        SetPreview(r0);
+    }
+}
+
+__attribute__((section(".text.Set2ndTabPath")))
+void Set2ndTabPath(void *r0, WSHDR *ws) {
+    _wsprintf(ws, "1:\\");
+}
