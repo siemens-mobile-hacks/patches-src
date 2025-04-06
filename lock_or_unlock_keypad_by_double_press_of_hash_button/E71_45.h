129ac920a6cd18697f07579b1d321054b507e36b
Philip Minakov <sir.cadogan@protonmail.com>
2025-04-06 19:30:56 +0400

    Add patch

---
+++ lock_or_unlock_keypad_by_double_press_of_hash_button/E71_45.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define ADDR_strncmp       0xA122B001
+#define ADDR_GeneralFuncF1 0xA094F597
+
+#define ADDR_KeypadLock (0xA06E8372 | 1)
---
+++ lock_or_unlock_keypad_by_double_press_of_hash_button/patch.c
@@ -0,0 +1,23 @@
+#include <swilib.h>
+
+#ifdef NEWSGOLD
+    #ifdef E71_45
+        #include "E71_45.h"
+    #endif
+#endif
+
+#define _strncmp ((int (*)(const char *, const char *, size_t))(ADDR_strncmp))
+#define _GeneralFuncF1 ((void (*)(int))(ADDR_GeneralFuncF1))
+
+#define KeypadLock ((void (*)())(ADDR_KeypadLock))
+
+__attribute__((target("thumb")))
+__attribute__((section(".text.KbdLock_Hook")))
+int KbdLock_Hook(const char *s1, const char *s2, size_t n) {
+    if (s1[0] == '#' && s1[1] == '#') {
+        _GeneralFuncF1(1);
+        KeypadLock();
+        return -1;
+    }
+    return _strncmp(s1, s2, n);
+}
