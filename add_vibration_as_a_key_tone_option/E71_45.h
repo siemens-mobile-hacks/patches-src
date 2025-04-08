480626a1c46a2babedce8453c81825f8f31b4d68
Philip Minakov <sir.cadogan@protonmail.com>
2025-04-08 14:52:56 +0400

    Update patch
    add_vibration_as_a_key_tone_option

--- add_vibration_as_a_key_tone_option/E71_45.h
+++ add_vibration_as_a_key_tone_option/E71_45.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#define ADDR_wsprintf         0xA093EB4D
+#define ADDR_PlayVibra        0xA006F131
+#define ADDR_CreateLocalWS    0xA04FB25B
+#define ADDR_GetLangMesIntoWS 0xA08D5F30
+
+#define ADDR_ClearLgp   (0xA095D6FC)
+#define ADDR_SetLgpTextWS (0xA095D468)
+
 #define ADDR_GetKeyTone    (0xA05A4B08)
 #define ADDR_GetVibraState (0xA04ADD30 | 1)
-
-#define ADDR_PlayVibra    0xA006F131
--- add_vibration_as_a_key_tone_option/patch.c
+++ add_vibration_as_a_key_tone_option/patch.c
@@ -1,3 +1,4 @@
+#include <swilib.h>
 #include <swilib/patch.h>
 
 #ifdef NEWSGOLD
@@ -6,17 +7,37 @@
     #endif
 #endif
 
+
+#define _wsprintf ((void (*)(WSHDR *, const char *, ...))(ADDR_wsprintf))
+#define _PlayVibra ((void (*)(int, int, int))(ADDR_PlayVibra))
+#define _CreateLocalWS ((void (*)(WSHDR *, uint16_t *, int))(ADDR_CreateLocalWS))
+#define _GetLangMesIntoWS ((void (*)(int, WSHDR *))(ADDR_GetLangMesIntoWS))
+
+#define ClearLgp     ((void (*)(int))(ADDR_ClearLgp))
+#define SetLgpTextWS ((void (*)(int, const WSHDR *))(ADDR_SetLgpTextWS))
+
+
+__attribute__((target("thumb")))
+__attribute__((section(".text.AddLgpMessage_Hook")))
+void AddLgpMessage_Hook(int lgp_id, WSHDR *dest) {
+    WSHDR ws;
+    uint16_t wsbody[64];
+    _CreateLocalWS(&ws, wsbody, 64);
+    _wsprintf(&ws, "%t + %t", 0x17B, 0x1D2);
+    SetLgpTextWS(0x7FFFD000, &ws);
+    _GetLangMesIntoWS(lgp_id, dest);
+}
+
 #define GetKeyTone ((int (*)())(ADDR_GetKeyTone))
 #define GetVibraState ((int (*)())(ADDR_GetVibraState))
-
-#define _PlayVibra ((void (*)(int, int, int))(ADDR_PlayVibra))
 
 __attribute__((target("thumb")))
 __attribute__((section(".text.PlayKeySound_Hook")))
 int PlayKeySound_Hook(void) {
     int key_tone = GetKeyTone();
-    if (key_tone == 3) { // vibration
+    if (key_tone == 3 || key_tone == 4) { // vibration
         _PlayVibra(50, 5, 1);
+        return key_tone - 3;
     }
     return key_tone;
 }
