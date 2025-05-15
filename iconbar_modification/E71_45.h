2fa34542710f582285b6a4b5f46e662cf672192d
Philip Minakov <sir.cadogan@protonmail.com>
2025-05-15 23:23:52 +0400

    Update IconBar Modification

--- iconbar_modification/E71_45.h
+++ iconbar_modification/E71_45.h
@@ -1,10 +1,17 @@
 #pragma once
 
-#define ADDR_DrawImg         0xA05C17C5
-#define ADDR_DrawCanvas      0xA05C0B9F
-#define ADDR_GetImgWidth     0xA094CFD3
-#define ADDR_GetImgHeight    0xA094CFC3
+#define ADDR_DrawImg                   0xA05C17C5
+#define ADDR_GetTime_ws                0xA08D0951
+#define ADDR_DrawCanvas                0xA05C0B9F
+#define ADDR_DrawString                0xA094A3AB
+#define ADDR_GetImgWidth               0xA094CFD3
+#define ADDR_GetImgHeight              0xA094CFC3
+#define ADDR_GetFontYSIZE              0xA0A32378
+#define ADDR_Get_WS_width              0xA08D2EEF
+#define ADDR_CreateLocalWS             0xA04FB25B
+#define ADDR_GetPaletteAdrByColorIndex 0xA0948031
 
+#define ADDR_PutTimeIntoWS  (0xA05C3F20 | 1)
 #define ADDR_GetNetworkIcon (0xA05C0E1E | 1)
 #define ADDR_GetBatteryIcon (0xA05C0BD4 | 1)
 #define ADDR_RamIconBarList (0xA8E3750C)
--- iconbar_modification/patch.c
+++ iconbar_modification/patch.c
@@ -9,8 +9,14 @@
 
 #define _DrawImg ((void (*)(int, int, int))(ADDR_DrawImg))
 #define _DrawCanvas ((void (*)(void *, int, int, int, int, int))(ADDR_DrawCanvas))
+#define _DrawString ((void (*)(const WSHDR *, int, int, int, int, int, int, const char *, const char *))(ADDR_DrawString))
+#define _GetTime_ws ((void (*)(WSHDR *, TTime,
 #define _GetImgWidth ((int (*)(int))(ADDR_GetImgWidth))
 #define _GetImgHeight ((int (*)(int))(ADDR_GetImgHeight))
+#define _Get_WS_width ((int (*)(const WSHDR *, int))(ADDR_Get_WS_width))
+#define _GetFontYSIZE ((int (*)(int))(ADDR_GetFontYSIZE))
+#define _CreateLocalWS ((void (*)(WSHDR *, uint16_t *, int))(ADDR_CreateLocalWS))
+#define _GetPaletteAdrByColorIndex ((const char *(*)(int))(ADDR_GetPaletteAdrByColorIndex))
 
 __attribute__((always_inline))
 inline int GetIconBarIcon(int n) {
@@ -23,31 +29,51 @@ void DrawIconBarIcon(int x, int picture) {
     _DrawImg(x, (24 - _GetImgHeight(picture)) / 2, picture);
 }
 
+#define PutTimeIntoWS ((void (*)(WSHDR *))(ADDR_PutTimeIntoWS))
 #define GetNetworkIcon ((int (*)())(ADDR_GetNetworkIcon))
 #define GetBatteryIcon ((int (*)())(ADDR_GetBatteryIcon))
 
 __attribute__((target("thumb")))
-__attribute__((section(".text.DrawIcons_Hook")))
-void DrawIcons_Hook(int icons_n, void *data) {
+__attribute__((section(".text.DrawIconBar_Hook")))
+void DrawIconBar_Hook(int icons_n, void *data) {
+    int x = 0; int y = 0; int x2 = 0; int y2 = 0;
     _DrawCanvas(data, 0, 0, 239, 23, 1);
+
+    WSHDR ws;
+    uint16_t wsbody[16];
+    _CreateLocalWS(&ws, wsbody, 16);
+    PutTimeIntoWS(&ws);
+    x = 1;
+    y = (24 - _GetFontYSIZE(FONT_SMALL)) / 2;
+    x2 = x + _Get_WS_width(&ws, FONT_SMALL);
+    _DrawString(&ws, x, y, x2, y + _GetFontYSIZE(FONT_SMALL), FONT_SMALL, 0,
+                _GetPaletteAdrByColorIndex(PC_FOREGROUND), _GetPaletteAdrByColorIndex(0x17));
 
     int w = 0;
     uint16_t picture;
     int network_icon = GetNetworkIcon();
     int battery_icon = GetBatteryIcon();
-    for (int i = 0 ; i < icons_n; i++) {
-        picture = GetIconBarIcon(i);
-        w += _GetImgWidth(picture) + 8;
-    }
     if (network_icon != 0xFFFF) {
         w += _GetImgWidth(network_icon) + 8;
     }
     if (battery_icon != 0xFFFF) {
         w += _GetImgWidth(battery_icon) + 8;
     }
+
+    int max_icons = 0;
+    for (int i = 0 ; i < icons_n; i++) {
+        const int width = _GetImgWidth(GetIconBarIcon(i)) + 8;
+        w += width;
+        if (239 - w <= x2 + 4) {
+            w -= width;
+            break;
+        }
+        max_icons++;
+    }
+
     w -= 4;
-    int x = 239 - w;
-    for (int i = 0; i < icons_n; i++) {
+    x = 239 - w;
+    for (int i = 0; i < max_icons; i++) {
         picture = GetIconBarIcon(i);
         x += 4;
         DrawIconBarIcon(x, picture);
@@ -62,4 +88,3 @@ void DrawIcons_Hook(int icons_n, void *data) {
         DrawIconBarIcon(x + 4, battery_icon);
     }
 }
-
