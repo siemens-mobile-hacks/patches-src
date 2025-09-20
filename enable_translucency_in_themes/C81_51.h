28d164ffd5be243df28d7e03d6d3280f76b0f87d
Philip Minakov <sir.cadogan@protonmail.com>
2025-09-20 16:47:32 +0400

    Fix

--- enable_translucency_in_themes/C81_51.h
+++ enable_translucency_in_themes/C81_51.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define ADDR_Obs_SetInput_File  (0xA0D0491C)
+#define ADDR_Obs_SetInput_File  (0xA099E0BC)
 #define ADDR_Obs_SetTransparent (0xA0A64B88)
 
 #define ADDR_GetThemeCacheId (0xA05C6B60 | 1)
