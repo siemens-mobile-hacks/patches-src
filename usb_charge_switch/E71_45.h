c2fb0c65fea56ff7f498278864cd6e6c5fca158a
Philip Minakov <sir.cadogan@protonmail.com>
2026-01-18 18:26:17 +0400

    Add patch

---
+++ usb_charge_switch/E71_45.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define ADDR_IsAnyAccessoryConnected (0xA053E95C)
+
+#define ADDR_Set      (0xA04CB7A4)
+#define ADDR_GetState (0xA00945A0 | 1)
+
+#define ADDR_RamAccessoriesFlags (uint32_t*)(0xA8D9424C)
+
+#define ACCESSORIES (uint8_t*)(0xA1381500)
---
+++ usb_charge_switch/patch.c
@@ -0,0 +1,42 @@
+#include <swilib.h>
+
+#ifdef NEWSGOLD
+    #ifdef E71_45
+        #include "E71_45.h"
+    #endif
+#endif
+
+#define _IsAnyAccessoryConnected ((int (*)(uint8_t *, uint8_t))(ADDR_IsAnyAccessoryConnected))
+
+#define IsDisabledUSBCharging() !((*ADDR_RamAccessoriesFlags) & (1U << 21))
+
+#define Set ((int (*)(void *, int, int))(ADDR_Set))
+
+__attribute__((target("thumb")))
+__attribute__((section(".text.Hook_1")))
+int Hook_1(void *r0, int r1, int r2) {
+    if (IsDisabledUSBCharging()) {
+        if (_IsAnyAccessoryConnected(ACCESSORIES, 6)) {
+            return 0;
+        }
+    }
+    return Set(r0, r1, r2);
+}
+
+#define GetState ((int (*)(void *))(ADDR_GetState))
+
+__attribute__((target("thumb")))
+__attribute__((section(".text.Hook_2")))
+int Hook_2(void *r0) {
+    int state = GetState(r0);
+    if (IsDisabledUSBCharging()) {
+        if (state == 1) {
+            state = 0;
+        } else if (state == 2) {
+            if (_IsAnyAccessoryConnected(ACCESSORIES, 6)) {
+                state = 0;
+            }
+        }
+    }
+    return state;
+}
